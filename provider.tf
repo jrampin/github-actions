295bd492489bc5b38b6b1906c24de452cd9896ec
Jhonatan Rampin <jhonatan.rampin@crayon.com>
2024-02-24 16:01:30 +1300

    Testing

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/azurerm"
-#       version = "~> 3.93.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.93.0"
+    }
+  }
+}
 
 provider "azurerm" {
   features {}
