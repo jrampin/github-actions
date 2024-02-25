b22b06555c6adcd89db15322cb758dcd653370ec
Jhonatan Rampin <jhonatan.rampin@fbu.com>
2024-02-25 15:34:30 +1300

    Initial commit

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+# Module for Service Principals
+data "azuread_client_config" "current" {}
+
 resource "azurerm_resource_group" "main" {
   name     = "rg-${var.environment}"
   location = "australiaeast"
--- provider.tf
+++ provider.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.93.0"
     }
+
+    azuread = {
+      source = "hashicorp/azuread"
+      # version = "~>2.0"
+    }
   }
 }
 
